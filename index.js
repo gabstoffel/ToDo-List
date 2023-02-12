43c5767cac49cd6c57e956f38f5ed27b52908fb7
Gabriel Stoffel <gc.stoffel03@gmail.com>
2023-02-12 14:31:21 -0300

    version 1

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ import apiRoutes from './routes/api.js';
 import path from 'path';
 const app = express();
 import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
-
 let PORT = 3030;
-
 app.use('/api', apiRoutes);
 app.use(express.static(path.join(__dirname, "client")));
 app.listen(PORT, () => {
